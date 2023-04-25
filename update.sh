b8dc039dd35c0177cef16753bb761fcaca5b03db
’Synge <wistaria@phys.s.u-tokyo.ac.jp>
2023-04-25 09:40:34 +0900

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
-CMAKE_FILES=$(ls *.cmake | sort)
+CMAKE_FILES=$(ls cmake/*.cmake | sort)
 for f in ${CMAKE_FILES}; do
-  if test -f ${SCRIPT_DIR}/cmake/$f; then
+  if test -f ${SCRIPT_DIR}/$f; then
     echo "update: $f"
-    cp -f ${SCRIPT_DIR}/cmake/$f $f
+    cp -f ${SCRIPT_DIR}/$f $f
   else
     echo "skip: $f"
   fi
